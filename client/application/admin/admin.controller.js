97938637c994576936196d2c79473f90bd4fa253
Andrey <ultra-777@yandex.ru>
2015-12-24 04:02:03 +0300

    some

--- client/application/admin/admin.controller.js
+++ client/application/admin/admin.controller.js
@@ -8,10 +8,32 @@ angular
         '$timeout',
         '$location',
         '$window',
-        'dataServiceImpl',
+        'adminService',
 function (scope, rootScope, timeout, location, window, dataService) {
 
     var vm = this;
     vm.data = 'admin data';
+    vm.account = null;
+
+    function initialize(){
+
+
+        dataService
+            .getAccountInfo()
+            .then(function(result){
+                vm.account = result;
+                var prefix = '::ffff:';
+                var ipCandidate = (result.ip) ? result.ip.toString() : '';
+                var ip = ((ipCandidate.indexOf(prefix) > -1) ? ipCandidate.substring(prefix.length, ipCandidate.length) : ipCandidate);
+                if (ip)
+                    vm.account.ip = ip;
+                //initializeMenu();
+            }, function(err){
+                var q = 99;
+            });
+
+    }
+
+    initialize();
 
 }]);
