08f883d3a98fe01e7fe0cc9f80c1e74a22a363bd
Andrey <ultra-777@yandex.ru>
2015-12-25 03:34:47 +0300

    history agent

---
+++ server/models/storage/history/agent.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Andrey on 21.12.2014.
+ */
+
+"use strict";
+
+function model(sequelize, DataTypes) {
+    var definition =
+        sequelize.define(
+            "session",
+            {
+                id: { type: DataTypes.STRING(255), primaryKey: true, allowNull: false },
+                data: { type: DataTypes.JSON },
+                addressInitial: { type: DataTypes.STRING(256) },
+                addressLast: { type: DataTypes.STRING(256), allowNull: true },
+                created: { type: DataTypes.DATE },
+                updated: { type: DataTypes.DATE, allowNull: true }
+            },
+            {
+                // don't add the timestamp attributes (updatedAt, createdAt)
+                timestamps: false,
+
+                schema: "security",
+
+                tableName: 'Sessions'
+            }
+        );
+    return definition;
+};
+
+
+module.exports = {
+    model: model
+};
