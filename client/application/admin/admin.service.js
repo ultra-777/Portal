1868659be0439c9a9f1189d23487727ea58396d2
Andrey <ultra-777@yandex.ru>
2015-12-29 03:21:43 +0300

    some

--- client/application/admin/admin.service.js
+++ client/application/admin/admin.service.js
@@ -14,6 +14,34 @@ angular
 					null);
 		};
 
+		this.pull = function () {
+			return data.httpRequest(
+					'POST',
+					'admin/pull',
+					null);
+		};
+
+		this.install = function () {
+			return data.httpRequest(
+					'POST',
+					'admin/install',
+					null);
+		};
+
+		this.build = function () {
+			return data.httpRequest(
+					'POST',
+					'admin/build',
+					null);
+		};
+
+		this.restart = function () {
+			return data.httpRequest(
+					'POST',
+					'admin/restart',
+					null);
+		};
+
 		this.reboot = function () {
 			return data.httpRequest(
 					'POST',
