268d1e15dd667335047a1708a0ffe25a4ce3513b
Andrey <ultra-777@yandex.ru>
2016-01-04 14:43:46 +0300

    admin update

---
+++ client/application/admin/update/admin.update.service.js
@@ -0,0 +1,52 @@
+'use strict';
+
+//Menu service used for managing  menus
+angular
+	.module('admin')
+	.service('adminUpdateService', [
+		'commonMvcServiceImpl',
+	function(data) {
+
+		this.getAccountInfo = function () {
+			return data.httpRequest(
+					'POST',
+					'security/getAccountInfo',
+					null);
+		};
+
+		this.pull = function () {
+			return data.httpRequest(
+					'POST',
+					'update/pull',
+					null);
+		};
+
+		this.install = function () {
+			return data.httpRequest(
+					'POST',
+					'update/install',
+					null);
+		};
+
+		this.build = function () {
+			return data.httpRequest(
+					'POST',
+					'update/build',
+					null);
+		};
+
+		this.restart = function () {
+			return data.httpRequest(
+					'POST',
+					'update/restart',
+					null);
+		};
+
+		this.reboot = function () {
+			return data.httpRequest(
+					'POST',
+					'update/reboot',
+					null);
+		};
+	}
+]);
