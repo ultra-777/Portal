310796bc258cc5878dee1cda74898ac1a3122f83
Andrey <ultra-777@yandex.ru>
2016-01-06 05:02:05 +0300

    repository

--- server/controllers/repository.js
+++ server/controllers/repository.js
@@ -1,66 +1,63 @@
 'use strict';
-var exec = require('child_process').exec;
 
-exports.pull = function(req, res) {
-    execute(
-        'git pull',
-        function(callback){
-            res.jsonp(callback);
+var repositoryImpl = require('../models/repository');
+
+exports.find = function(req, res) {
+    repositoryImpl
+        .find(req.body.name, function(instances, error){
+            if (error)
+                res.status(500).send({ error: error, message: error.message });
+            else{
+                if (!instances)
+                    instances = [];
+
+                res.send(instances);
+            }
         });
 };
 
-exports.install = function(req, res) {
-    execute('npm install',
-        function(callback){
-            res.jsonp(callback);
-    });
-};
-
-exports.build = function(req, res) {
-    execute('grunt production',
-    function(callback){
-        res.jsonp(callback);
-    });
-};
-
-exports.restart = function(req, res) {
-    var line = 'reboot';
-    console.log(line);
-    execute(line,
-        function(callback1){
-            console.log(callback1);
-            res.jsonp(callback1);
-    });
-};
-
-
-function execute(command, callback){
-    var startMoment = (new Date()).toUTCString();
-    exec(command,
-        {
-            encoding: 'utf8',
-            timeout: 0,
-            maxBuffer: 1024*1024,
-            killSignal: 'SIGTERM',
-            cwd: null,
-            env: null
-        },
-        function(error, stdout, stderr){
-
-            var stopMoment = (new Date()).toUTCString();
-
-            var output = 'started: ' + startMoment;
-            if (stdout)
-                output = output + '\r\n\r\n' + stdout;
-
-            if (stderr)
-                output = output + '\r\n' + stderr;
-
-            output = output + '\r\n\r\ncomplete: ' + stopMoment;
-
-
-            callback(output);
+exports.get = function(req, res) {
+    repositoryImpl
+        .get(req.body.id, function(instance, error){
+            if (error)
+                res.status(500).send({ error: error, message: error.message });
+            else{
+                res.send(instance ? instance.toJson() : null);
+            }
         });
-}
+};
+
+exports.update = function(req, res) {
+    repositoryImpl
+        .update(req.body.id, req.body.name, req.body.location, req.body.isOpen, function(instance, error){
+            if (error)
+                res.status(500).send({ error: error, message: error.message });
+            else{
+                res.send(instance ? instance.toJson() : null);
+            }
+        });
+};
+
+exports.create = function(req, res) {
+    repositoryImpl
+        .create(req.body.name, req.body.location, req.body.isOpen, function(instance, error){
+            if (error)
+                res.status(500).send({ error: error, message: error.message });
+            else{
+                res.send(instance ? instance.toJson() : null);
+            }
+        });
+};
+
+exports.delete = function(req, res) {
+    repositoryImpl
+        .delete(req.body.id, function(result, error){
+            if (error)
+                res.status(500).send({ error: error, message: error.message });
+            else{
+                res.send(result);
+            }
+        });
+};
 
 
